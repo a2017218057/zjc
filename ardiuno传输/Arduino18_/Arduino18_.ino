cfd8de33fd648b9af04853fba13f6dc74e09d118
Machi <793919316@qq.com>
2017-10-27 23:10:23 +0800

    add timer

--- ardiuno传输/Arduino18_/Arduino18_.ino
+++ ardiuno传输/Arduino18_/Arduino18_.ino
@@ -1,4 +1,5 @@
 #include <avr/pgmspace.h>
+#include <TimerOne.h>
 String inputString ="";         // 缓存字符串
 boolean stringComplete = false;  // 是否string已经完成缓存
 boolean isWork = false;//标志cantool虚拟装置是否在运行
@@ -7,13 +8,21 @@ boolean ledisOn = false;
 const char sA[] PROGMEM="BO_ 801 HVAC_2: 8 HVAC";
 const PROGMEM char ss[]  = {"BO_ 801 HVAC_2: 8 HVAC SG_ HVAC_RawCabinTemp : 7|8@0+ (0.5,-40) [-40|87.5] \"°C\"  CDU SG_ HVAC_CorrectedCabinTemp : 15|8@0+ (0.5,-40) [-40|87.5] \"°C\"  CDU SG_ HVAC_RawCabinTempVD : 19|1@0+ (1,0) [0|1] \"\"  CDU SG_ HVAC_CompressorComsumpPwr : 17|10@0+ (10,0) [0|8000] \"w\"  BCM SG_ HVAC_PTCPwrAct : 33|10@0+ (10,0) [0|8000] \"w\"  BCM  SG_ HVAC_stPTCAct : 55|3@0+ (1,0) [0|1] \"\"  BCM SG_ HVAC_CorrectedCabinTempVD : 18|1@0+ (1,0) [0|1] \"\" CDU "};
 const PROGMEM char stdtt[] = {"t31D80100000000000000t320880478C2F05A1D29At360800402418E4000000t39380000381403000000t03D80D00000000000000t42B84215640000000001t3208094697860945675Dt320838D1DB1304806D85t31880300000000000000t34580000000006CA0000t31D80200000000000000t31880100000000000000t03D83000000000000000t32180F23042701722000t36489476B18400000000t39380000070701000000t36380C51C0521B0090D9t03D80200000000000000t42080332510000000000t345800000000075F0000t31D80600000000000000t393800002B3521000000t03D82800000000000000t31880100000000000000t39380000362001000000t36382150014720000009t39380000030D22000000t3638441F413B5600908Dt42082802860000000000t3588423C5015130EE000t31D80300000000000000t31880200000000000000t320810F35FB10762018At36483274950000000000t31D80600000000000000t364879328CC200000000t03D83400000000000000t31D80300000000000000t3608043D0622AE000000t03D83200000000000000t3218AF090E1602850000t3458000000002BF00000t42B81F33A30000000000t3208F0EE8D2303035783t4208E233D40000000001t393800003A0510000000t35884B10B06F17100000t31D80200000000000000t31D80500000000000000t03D83000000000000000t31D80500000000000000t3648C44D930400000000t31D80400000000000000t42084017D70000000001t42B83703C90000000001t31D80600000000000000t345800000000280B0000t3648D637328700000000t360807462D1EC7000000t42B89632F50000000001t3208B978B34B04A715C1t363819A7406546008012t31880100000000000000t3218A6F90C7300DB0000t36380C1980A01400508Ct32184FE1096F00540000t42082A34E90000000001t393800001D3320000000t3608024A323EBA000000t36480453270200000000t31D80000000000000000t31D80000000000000000t32189A25002B02042000t3588892C001B140CB000t42B87B373F0000000001t345800000000212F0000t3208615350C50C4645B6t3608073B2639A7000000t3588423C74111604F000t31D80300000000000000t42088D20800000000001t3458000000000D3A0000t31D80400000000000000t42B85F125E0000000000t03D80700000000000000t360804103E15FB000000t36382D5580BA460080F3t36080704255DAD000000t320871AD8DAD05831CC7t31880200000000000000t393800000A1723000000t31880300000000000000t42081606020000000001t3208809D240703C4869At32184CE602DC017F2000t42B8FD12570000000001t39380000302023000000"};
+const PROGMEM char stdttt[] = {"t32080000000000000000,t32080128000000000000,t32080050000000000000,t32080178000000000000,t320800A0000000000000,t320801FF000000000000,t3608000E00009B000000,t3608042B0000C3000000,t360800410000EB000000,t3608024A0000FF000000,t3FC18400000000000000,t3FC10600000000000000,t11180000000000000000,t11180000000000009001,t11180000000000005802,t0648F01732C30000882C,t0648101B32DC00006432,t11180000000000009001,t35880100003F000C0000,t35880000007D00180000,t34580000000010AA0000,t393800003B0000000000,t39380000330000000000,t320800F0000000000000,t0648131428A300008025,t0648101B32DC00006432,t0648401F64FF0000983A,t0648B00A14570000EC13"};
+const long interval = 100000; //  100000 microseconds = 100 ms = 0.1 second
 int readmsg_tt = 0;
 char s[] =  "rdfghjklfgfydushijfguerhfdud";
 String p[7];
 boolean sendmsg_tt = false;
 boolean reread = false;
 int timeled = 0;
-int delta =0;
+float delta =0.0;
+void ledblink(){
+
+    
+ digitalWrite( 13, digitalRead( 13 ) ^ 1 ); // toggle 0, 1
+ 
+  }
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);//cantoolApp要求的频率
@@ -24,13 +33,14 @@ void setup() {
                                     //(板上连接GND__D2两个点模拟总线发送标准帧和扩展帧的事件)
   digitalWrite(2,HIGH);
   pinMode(5,INPUT);                 //将2号数字口设置为输入状态，13号数字口设置为输出状态
-                                    //(板上连接GND__D2两个点模拟总线发送标准帧和扩展帧的事件)
+                                 //(板上连接GND__D2两个点模拟总线发送标准帧和扩展帧的事件)
   digitalWrite(5,HIGH);
   inputString.reserve(200);
   pinMode(LED_BUILTIN, OUTPUT);
   int numofss = sizeof(ss)/sizeof(char);
  // read back a char
- 
+ Timer1.initialize( interval ); // 初始化,  interval 以 micro sec 為單位
+ Timer1.attachInterrupt( ledblink ); // attach the service routine here
 //String id = canmsg(ss);
  //Serial.print("id"+id);
 }
@@ -82,10 +92,27 @@ void loop() {
 /****************************************************/
 if(sendmsg_tt){
   delay(1000);
-  sendStdFrame_ctapp(stdtt);
+  sendStdFrame_ctapp(stdttt);
   //Serial.println(readmsg_tt);
   }
-if(ledisOn){
+  
+  if(ledisOn){
+ 
+  //Serial.print(delta/100.0 * interval );
+  
+  Timer1.setPeriod( delta/100.0 * interval );
+//  Timer1.attachInterrupt(delta/100.0 * interval);
+  //digitalWrite(LED_BUILTIN, LOW);
+//  continue;
+ //ledisOn =false;
+  }else{
+ //   Timer1.detachInterrupt(); // 停止閃爍
+    Timer1.stop();
+    timeled =0;
+    ledisOn =false;
+    }
+  
+/*if(ledisOn){
   if(timeled<30)
   timeled+=1;
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
@@ -98,7 +125,8 @@ if(ledisOn){
     }
     }
   
-
+*/
+}
 }
 
 /*
@@ -210,7 +238,7 @@ void sendExdFrame_ctapp(){
 void rec_stdfrm_rtn(String message){
   msg_rtn = "\r";
   serial_return(msg_rtn);
-  delay(100);//总感觉发太快
+  //delay(100);//总感觉发太快
   int len = message.length();
   String send_canmsg = "";
   String timenum = "";
@@ -227,8 +255,9 @@ void rec_stdfrm_rtn(String message){
   int t = Str_to_num(timenum);
   ledisOn = true;
   delta = t;
-  loopled(t);               //循环播放灯
+ // loopled(t);               //循环播放灯
  // Serial.println(t);
+ serial_return(" ");
   }
 
   //接收cantoolapp的扩展帧 复用标准帧的代码
